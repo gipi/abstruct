c224afdb2065dc91825599e981d55e1c52c541ea
Gianluca Pacchiella <gianluca.pacchiella@ktln2.org>
2018-02-24 21:06:02 +0100

    Rework Dependency.

--- abstruct/properties.py
+++ abstruct/properties.py
@@ -1,16 +1,26 @@
 
 
-class Dependencies(object):
+class Dependency(object):
     '''This makes the relation between fields possible.
 
     We want that accessing this field the resolution is automagical.
 
     
     '''
-    def __init__(self, expression):
+    def __init__(self, expression, obj=None):
         self.expression = expression
-        self.dst = None
-        self.src = None
+        self.obj = obj
+
+    def __call__(self, obj):
+        return Dependency(self.expression, obj=obj)
+
+    def resolve(self):
+        # here split 'other_child.field'
+        import ipdb;ipdb.set_trace()
+        other_child, field_name = self.expression.split('.')
+        sibiling = getattr(self.obj, other_child)
+
+        return getattr(sibiling, field_name).value
 
 
 # NOTE: we need the caller to seek() correctly a given offset

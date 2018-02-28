8d13a269ebec824ee569b3959ec515600d763e1a
Gianluca Pacchiella <gianluca.pacchiella@ktln2.org>
2018-02-28 20:16:34 +0100

    Add test executable.
    Make more uniform test possible.

--- abstruct/test.py
+++ abstruct/test.py
@@ -72,47 +72,20 @@ class FieldsTests(unittest.TestCase):
 
 
 class ELFTest(unittest.TestCase):
-    def setUp(self):
-        self.path = tempfile.mkdtemp()
-        logger.debug('creating temp path = \'%s\'' % self.path)
-
-    def tearDown(self):
-        logger.debug('removing path \'%s\'' % self.path)
-        shutil.rmtree(self.path)
-
     def test_empty(self):
         elf = ElfFile()
         self.assertEqual(elf.elf_header.e_type.value, 0x02)
 
     def test_32bits(self):
-        code = '''
-#include <stdio.h>
-
-int main() {
-    return 0;
-}
-'''
-        path_c_file = os.path.join(self.path, 'main.c')
-        path_exe    = os.path.join(self.path, 'main')
-        with open(path_c_file, 'w') as f:
-            f.write(code)
-
-        process = subprocess.Popen([
-            'gcc',
-            '-m32',
-            path_c_file,
-            '-o',
-            path_exe,
-        ])
-        process.wait()
+        path_elf = os.path.join(os.path.dirname(__file__), 'main')
 
         logger.info(subprocess.check_output([
             'readelf',
             '-h',
-            path_exe,
+            path_elf,
         ]).decode('utf-8'))
 
-        elf = ElfFile(path_exe)
+        elf = ElfFile(path_elf)
 
         self.assertEqual(elf.elf_header.e_type.value, 0x03)
         self.assertEqual(elf.elf_header.e_machine.value, 0x03)

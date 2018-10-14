c731abae7acb7b84941f6560a1649207438c2a19
Gianluca Pacchiella <gianluca.pacchiella@ktln2.org>
2018-10-14 18:21:35 +0200

    stk500: add analysis docstring.

--- abstruct/stk500.py
+++ abstruct/stk500.py
@@ -2,6 +2,15 @@
 Description of STK500 Communication protocol.
 
 http://ww1.microchip.com/downloads/en/AppNotes/doc2591.pdf
+
+ANALYSIS
+--------
+
+It's a little tricky since the communication is composed of request and
+response of command at application level.
+
+So we should maintain an internal status to tell which side of the communication
+we are packing/unpacking.
 '''
 from .core import *
 from . import fields
@@ -9,7 +18,7 @@ from . import fields
 class STK500Packet(Chunk):
     '''Transport layer'''
     message_start   = fields.StructField('B', default=0x1b, formatter='%02x')
-    sequence_number = fields.StructField('B')
+    sequence_number = fields.StructField('B') # TODO: create SequenceNumberField that creates such number
     message_size    = fields.StructField('H', little_endian=False)
     token           = fields.StructField('B', default=0x0e, formatter='%02x')
     message_body    = fields.StringField(Dependency('message_size'))

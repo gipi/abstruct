c9324f623f39aa04407f2e20291ce1a627be69ee
Gianluca Pacchiella <gianluca.pacchiella@ktln2.org>
2019-07-10 12:17:33 +0200

    ELF: add simple function to dump disassembled data.

--- setup.py
+++ setup.py
@@ -13,6 +13,9 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/gipi/abstruct",
     packages=setuptools.find_packages(),
+    install_requires=[
+        'capstone',
+    ],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: GPLv2 License",

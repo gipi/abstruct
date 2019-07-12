6c60a51248017629856bc3e4ae02a35328eca734
Gianluca Pacchiella <gianluca.pacchiella@ktln2.org>
2019-07-12 14:26:54 +0200

    ZIP: add ZipCompressMethod Flag.

--- abstruct/compression/zip.py
+++ abstruct/compression/zip.py
@@ -35,12 +35,30 @@ class ZIPFlags(Flag):
     MASK_HEADER             = 1 << 13
 
 
+class ZIPCompressionMethod(Flag):
+    NONE                = 0
+    SHRUNK              = 1
+    REDUCED_FACTOR_1    = 2
+    REDUCED_FACTOR_2    = 3
+    REDUCED_FACTOR_3    = 4
+    REDUCED_FACTOR_4    = 5
+    IMPLODED            = 6
+    RESERVED            = 7
+    DEFLATED            = 8
+    ENCHANCED_DEFLATED  = 9
+    PKWARE_DCL_IMPLODED = 10
+    BZIP2               = 12
+    LZMA                = 14
+    IBM_TERSE           = 18
+    IBM_LZ77            = 19
+    PPMd                = 98
+
 
 class ZIPHeader(Chunk):
     signature         = fields.StringField(0x4, default=b'PK\x03\x04')
     version           = fields.StructField('H')
     flags             = fields.BitField(ZIPFlags, 'H')
-    compression       = fields.StructField('H')
+    compression       = fields.BitField(ZIPCompressionMethod, 'H')
     modification_time = fields.StructField('H') # TODO: MS DOS field
     modification_date = fields.StructField('H')
     crc32             = fields.StructField('I') # TODO: CRC32 depending on data from file content

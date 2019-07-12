a05a9fedddda336256ddc32b409b0f4d40d08759
Gianluca Pacchiella <gianluca.pacchiella@ktln2.org>
2019-07-12 12:59:28 +0200

    Add BitField.

--- abstruct/compression/zip.py
+++ abstruct/compression/zip.py
@@ -15,15 +15,26 @@ The general structure is the following
 This format can be splitted in multiple medium (historically using floppy).
 
 '''
+from enum import Flag
 from ..core import Chunk
 from .. import fields
 from ..properties import Dependency
 
 
+class ZIPFlags(Flag):
+    ENCRIPTED = 0
+    COMPRESSION_OPTIONLSB = 1 << 1
+    COMPRESSION_OPTIONMSB = 1 << 2
+    DATA_DESCRIPTOR = 1 << 3
+    ENHANCED_DEFLATION = 1 << 4
+    COMPRESSED_PATCHED_DATA = 1 << 5
+
+
+
 class ZIPHeader(Chunk):
     signature         = fields.StringField(0x4, default=b'PK\x03\x04')
     version           = fields.StructField('H')
-    flags             = fields.StructField('H')
+    flags             = fields.BitField(ZIPFlags, 'H')
     compression       = fields.StructField('H')
     modification_time = fields.StructField('H') # TODO: MS DOS field
     modification_date = fields.StructField('H')

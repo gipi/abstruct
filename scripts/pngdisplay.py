85d6e2bcfc0ecc2ca89fe379cdfcba1757b7a1c0
Gianluca Pacchiella <gianluca.pacchiella@ktln2.org>
2019-09-20 20:34:47 +0200

    PNG: improve pngdisplay script.

--- scripts/pngdisplay.py
+++ scripts/pngdisplay.py
@@ -1,13 +1,20 @@
 '''
  $ convert -size 5x5 xc:red -size 5x5 xc:green -append show:
 '''
+import logging
 import sys
+import os
 import numpy as np
-from matplotlib import pyplot as plt
+from PIL import Image
 
 from abstruct.images.png import (
     PNGFile,
 )
+
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+logger.setLevel(level=logging.INFO if 'DEBUG' not in os.environ else logging.DEBUG)
 
 
 def usage(progname):
@@ -25,7 +32,7 @@ def get_scanline_data(scanline, width, depth):
     filter_type = scanline[:1]
     pixels = scanline[1:]
 
-    pixels = [BitArray(pixels)[_:_ + 2].int for _ in range(0, len(pixels) * 8, 2)]
+    pixels = [BitArray(pixels)[_:_ + depth].uint for _ in range(0, len(pixels) * 8, depth)]
 
     return filter_type, pixels[:width]
 
@@ -38,13 +45,27 @@ def iter_scanline(width, depth, idata, palette):
     # moreover has one byte for the filter type
     n_byte_scanline = math.ceil(width * depth / 8) + 1
 
+    logger.debug(f'iterating over scanlines for width {width} and depth {depth}')
     for scanline in iter_scanline_bits(idata, n_byte_scanline):
         ft, image_data = get_scanline_data(scanline, width, depth)
+
+        logger.debug(f'scanline: {ft} {image_data}')
+        if ft != b'\x00':
+            raise ValueError(f'Filter type {ft!r} not implemented')
         yield ft, image_data
 
 
 def pixels_from_image_data(ft, image_data, palette):
     return [palette[_].pixel for _ in image_data]
+
+
+def get_chunk_by_name(chunks, name):
+    chunk = list(filter(lambda x: x.type.value.decode() == name, chunks))
+
+    if len(chunk) == 0:
+        raise ValueError(f'no chunk with name {name}')
+
+    return chunk if len(chunk) > 1 else chunk[0]
 
 
 if __name__ == '__main__':
@@ -58,15 +79,12 @@ if __name__ == '__main__':
     for idx, chunk in enumerate(png.chunks.value):
         print(f'[{idx:02d}] {chunk!r}')
 
-    header = png.chunks.value[0].Data._field
-    palette = png.chunks.value[3].Data._field.value
-    data = png.chunks.value[7].Data._field.value
+    header  = get_chunk_by_name(png.chunks.value, 'IHDR').Data._field
+    palette = get_chunk_by_name(png.chunks.value, 'PLTE').Data._field.value
+    data    = get_chunk_by_name(png.chunks.value, 'IDAT').Data._field.value
     matrix = [pixels_from_image_data(ft, id, palette) for ft, id in iter_scanline(header.width.value, header.depth.value, data, palette)]
 
     pixels = np.array(matrix, dtype=np.uint8)
 
-    plt.imshow(pixels)
-    plt.show()
-
-    import ipdb
-    ipdb.set_trace()
+    image = Image.fromarray(pixels, 'RGB')
+    image.show()

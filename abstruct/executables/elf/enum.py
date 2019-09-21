1983f08ceb2031c85ef75b5240cc704b7cb78ffe
Gianluca Pacchiella <gianluca.pacchiella@ktln2.org>
2019-09-21 08:56:52 +0200

    ELF: add some missing numeric values.

--- abstruct/executables/elf/enum.py
+++ abstruct/executables/elf/enum.py
@@ -25,6 +25,178 @@ class ElfMachine(Enum):
     EM_88K   = 5
     EM_860   = 7
     EM_MIPS  = 8
+    EM_S370  = 9
+    EM_MIPS_RS3_LE = 10
+    EM_PARISC = 15
+    EM_VPP500 = 17
+    EM_SPARC32PLUS = 18
+    EM_960    = 19
+    EM_PPC    = 20
+    EM_PPC64  = 21
+    EM_S390   = 22
+    EM_SPU    = 23
+    EM_V800   = 36
+    EM_FR20   = 37
+    EM_RH32   = 38
+    EM_RCE    = 39
+    EM_ARM    = 40
+    EM_ALPHA  = 41
+    EM_SH     = 42
+    EM_SPARCV9 = 43
+    EM_TRICORE = 44
+    EM_ARC     = 45
+    EM_H8_300  = 46
+    EM_H8_300H = 47
+    EM_H8S     = 48
+    EM_H8_500  = 49
+    EM_IA_64   = 50
+    #EM_MIPS_X
+    #EM_COLDFIRE
+    #EM_68HC12
+    EM_MMA = 54
+    EM_PCP = 55
+    EM_NCPU = 56
+    EM_NDR1 = 57
+    EM_STARCORE = 58
+    EM_ME16 = 59
+    EM_ST100 = 60
+    EM_TINYJ = 61
+    EM_X86_64 = 62
+    EM_PDSP = 63
+    EM_PDP10 = 64
+    EM_PDP11 = 65
+    EM_FX66 = 66
+    EM_ST9PLUS = 67
+    EM_ST7 = 68
+    EM_68HC16 = 69
+    EM_68HC11 = 70
+    EM_68HC08 = 71
+    EM_68HC05 = 72
+    EM_SVX = 73
+    EM_ST19 = 74
+    EM_VAX = 75
+    EM_CRIS = 76
+    EM_JAVELIN = 77
+    EM_FIREPATH = 78
+    EM_ZSP = 79
+    EM_MMIX = 80
+    EM_HUANY = 81
+    EM_PRISM = 82
+    EM_AVR = 83
+    EM_FR30 = 84
+    EM_D10V = 85
+    EM_D30V = 86
+    EM_V850 = 87
+    EM_M32R = 88
+    EM_MN10300 = 89
+    EM_MN10200 = 90
+    EM_PJ = 91
+    EM_OPENRISC = 92
+    EM_ARC_COMPACT = 93
+    EM_XTENSA = 94
+    EM_VIDEOCORE = 95
+    EM_TMM_GPP = 96
+    EM_NS32K = 97
+    EM_TPC = 98
+    EM_SNP1K = 99
+    EM_ST200 = 100
+    EM_IP2K = 101
+    EM_MAX = 102
+    EM_CR = 103
+    EM_F2MC16 = 104
+    EM_MSP430 = 105
+    EM_BLACKFIN = 106
+    EM_SE_C33 = 107
+    EM_SEP = 108
+    EM_ARCA = 109
+    EM_UNICORE = 110
+    EM_EXCESS = 111
+    EM_DXP = 112
+    EM_ALTERA_NIOS2 = 113
+    EM_CRX = 114
+    #EM_XGATE
+    EM_C166 = 116
+    EM_M16C = 117
+    #EM_DSPIC30F
+    EM_CE = 119
+    EM_M32C = 120
+    EM_TSK3000 = 131
+    EM_RS08 = 132
+    '''
+    EM_SHARC
+    EM_ECOG2
+    EM_SCORE7
+    EM_DSP24
+    EM_VIDEOCORE3
+    EM_SE_C17
+    EM_TI_C6000
+    EM_TI_C2000
+    EM_TI_C5500
+    EM_TI_ARP32
+    EM_TI_PRU
+    EM_MMDSP_PLUS
+    EM_MMDSP_PLUS
+    EM_CYPRESS_M8C
+    EM_R32C = 162
+    #EM_TRIMEDIA
+    #EM_QDSP6
+    EM_8051 = 165
+    EM_STXP7X
+    EM_NDS32
+    EM_ECOG1
+    EM_ECOG1X
+    EM_MAXQ30
+    EM_XIMO16
+    #EM_MANIK
+    #EM_CRAYNV2
+    EM_RX = 173
+    #EM_METAG
+    #EM_MCST_ELBRUS
+    #EM_ECOG16
+    EM_CR16 = 177
+    EM_ETPU = 178
+    #EM_SLE9X
+    EM_L10M = 180
+    EM_K10M = 181
+    #EM_AARCH64
+    #EM_AVR32
+    EM_STM8 = 186
+    #EM_TILE64
+    #EM_TILEPRO
+    #EM_MICROBLAZE
+    EM_CUDA = 190
+    #EM_TILEGX
+    #EM_CLOUDSHIELD
+    #EM_COREA_1ST
+    #EM_COREA_2ND
+    #EM_ARC_COMPACT2
+    #EM_OPEN8
+    EM_RL78 = 197
+    #EM_VIDEOCORE5
+    #EM_78KOR
+    #EM_56800EX
+    EM_BA1 = 201
+    EM_BA2 = 202
+    #EM_XCORE
+    #EM_MCHP_PIC
+    #EM_INTEL205
+    #EM_INTEL206
+    #EM_INTEL207
+    #EM_INTEL208
+    #EM_INTEL209
+    EM_KM32 = 210
+    #EM_KMX32
+    #EM_KMX16
+    EM_KMX8 = 213
+    #EM_KVARC
+    EM_CDP = 215
+    EM_COGE = 216
+    EM_COOL = 217
+    EM_NORC = 218
+    #EM_CSR_KALIMBA
+    EM_Z80 = 220
+    #EM_VISIUM
+    EM_FT32 = 222'''
 
 
 class ElfVersion(Enum):
@@ -144,6 +316,7 @@ class ElfSymbolBindType(Enum):
     STB_LOCAL  = 0x00
     STB_GLOBAL = 0x01
     STB_WEAK   = 0x02
+    STB_NUM    = 0x03
 
 
 class ElfSymbolType(Enum):

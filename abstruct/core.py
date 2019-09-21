984b9e3a5e18721a28787b8ca1f52f90c77bc4ad
Gianluca Pacchiella <gianluca.pacchiella@ktln2.org>
2019-09-21 09:18:40 +0200

    core: add more useful exception.

--- abstruct/core.py
+++ abstruct/core.py
@@ -86,7 +86,10 @@ class Chunk(metaclass=MetaChunk):
 
         for name, field_constructor in self.__class__._meta.fields:
             logger.debug('field \'%s\' initialized' % name)
-            setattr(self, name, field_constructor(self, name=name))
+            try:
+                setattr(self, name, field_constructor(self, name=name))
+            except AttributeError as e:
+                raise AttributeError(f'field \'{name}\' cannot be set')
 
         # now we have setup all the fields necessary and we can unpack if
         # some data is passed with the constructor

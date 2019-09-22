2c9d123be13f5702dd4e4d39db6be86f2809c426
Gianluca Pacchiella <gianluca.pacchiella@ktln2.org>
2019-09-22 21:48:55 +0200

    Update dependency.

--- setup.py
+++ setup.py
@@ -15,6 +15,8 @@ setuptools.setup(
     packages=setuptools.find_packages(),
     install_requires=[
         'capstone',
+        'bitstring',
+        'pillow',
     ],
     classifiers=[
         "Programming Language :: Python :: 3",

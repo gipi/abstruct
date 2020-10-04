891f43792b77c93b9acde6d12a74b4d450e4af1c
Gianluca Pacchiella <gp@ktln2.org>
2020-10-04 18:08:46 +0200

    fix

--- abstruct/fields.py
+++ abstruct/fields.py
@@ -321,6 +321,8 @@ class StringField(Field):
         super().__init__(**kw)
         self._n = n
 
+        self.init()
+
     def __repr__(self):
         return '<%s(%s)>' % (self.__class__.__name__, repr(self.value))
 
